3a3cbe0aba8e1beb5dcba3d6565996c81ed5895d
cwb110581879 <40221020+cwb110581879@users.noreply.github.com>
2020-12-02 11:29:21 +0800

    First upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cb0dbde13ddd7966dcf259f6ead2ffc6.js"
+  "/precache-manifest.c577fb4f3558f9de5d6572ef28455b41.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "app"});
